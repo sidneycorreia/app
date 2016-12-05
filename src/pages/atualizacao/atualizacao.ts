15a6cc105b979083bec18679d2115db1e7bc3a1b
Sidney Correia <sidneycorreia@gmail.com>
2016-12-05 16:49:11 -0300

    modificacoes

--- src/pages/atualizacao/atualizacao.ts
+++ src/pages/atualizacao/atualizacao.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AtualizacaoService } from '../../providers/atualizacao-service';
+
 
 /*
   Generated class for the Atualizacao page.
@@ -9,14 +11,28 @@ import { NavController } from 'ionic-angular';
 */
 @Component({
   selector: 'page-atualizacao',
-  templateUrl: 'atualizacao.html'
+  templateUrl: 'atualizacao.html',
+  providers: [AtualizacaoService]
 })
 export class AtualizacaoPage {
+  
+  movies: Array<any>;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, private atualizacaoService: AtualizacaoService) {}
 
   ionViewDidLoad() {
     console.log('Hello AtualizacaoPage Page');
+
+    this.atualizacaoService.getAtualizacao('2016-02-01').subscribe(
+      data => {
+        this.movies = data.results; 
+        console.log(data);
+      },
+      err => {
+        console.log(err);
+      },
+      () => console.log('Movie Search Complete')
+    );
   }
 
 }
